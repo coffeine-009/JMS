44016fedcf73d0a7e62c2b5ba451a8426d3888a5
coffeine-009 <vitaliyacm@gmail.com>
2013-04-03 20:30:06 +0200

    Layout(HTML[base]+CSS[begin])2

---
+++ application/controllers/AuthorizationController.php
@@ -0,0 +1,53 @@
+<?php
+
+class AuthorizationController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function registrationAction()
+    {
+        // action body
+    }
+
+    public function registrationsuccessAction()
+    {
+        // action body
+    }
+
+    public function loginAction()
+    {
+        // action body
+    }
+
+    public function logoutAction()
+    {
+        // action body
+    }
+
+    public function forgotAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
