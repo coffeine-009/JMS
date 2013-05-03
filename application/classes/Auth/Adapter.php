8f3d90f0fd6b50dec4b0ac48f70a2170b4306d8d
Vitaliy <vitaliyacm@gmail.com>
2013-05-03 23:28:47 +0300

    Authorization + comment

--- application/classes/Auth/Adapter.php
+++ application/classes/Auth/Adapter.php
@@ -1,70 +1,70 @@
 <?php
+///	***	Class :: Adapter :: Authorization	***	***	***	***	***	***	***	***	///
 
+	/**	***	***	***	***	***	***	***	***	***	***	***	***	***	***	***	*
+	 * 																*
+	 * @copyroght 2013
+	 * 		by
+	 * 	@author Vitaliy Tsutsman
+	 * 
+	 * @date 2013-05-03 23:13:18 :: 2013-05-03 23:15:00
+	 * 
+	 * @address Ukraine/Petranka/Gryshevskii/234
+	 * 
+	 * @description
+	 * 	Auth adapter used ORM Doctrine.
+	 * 																*
+	*///***	***	***	***	***	***	***	***	***	***	***	***	***	***	***	*
+
+///	***	Code	***	***	***	***	***	***	***	***	***	***	***	***	***	***	***	///
 class Auth_Adapter
 	implements
 		Zend_Auth_Adapter_Interface
-{
-	///	***	Constants	***	///
-	const AUTH = 
-	"SELECT 
-		user.id, 
-		role.title AS role, 
-		email.contact 
-	FROM 
-		(user 
-			LEFT JOIN 
-		email 
-			ON( user.id = email.id_user )
-		)
-			LEFT JOIN 
-		role 
-			ON( role.id = user.id_role ) 
-	WHERE 
-		email.contact = '%s' 
-		AND 
-		user.password = MD5( '%s' ) 
-	LIMIT 
-		1
-	";
-		
+{		
 	///	***	Properties	***	///
 	protected $userData;
 	
 	///	***	Methods		***	///
-	public function __construct( /*string*/$UserName, /*string*/$Password )
+	public function __construct( 
+		/*string*/$UserName, 
+		/*string*/$Password 
+	)
 	{
 		//- Set properties -//
 		$this -> username = $UserName;
 		$this -> password = $Password;
 	}
 	
+	//- Authenticate -//
 	public function authenticate()
-	{		
+	{
+		//- Query to DB -//
 		$response = Doctrine_Query :: create()
+			//- First query -//
 			/*-> from( "Jms_Email e" )
 			-> leftJoin( "e.User u" )
 			-> leftJoin( "u.Role" )*/			
-			//- TMP -//
+			//- Better query -//
 			-> from( "Jms_User u" )
 			-> addFrom( "u.Email e" )
 			-> addFrom( "u.Role r" )
-			
+			//- Where -//
 			-> where( 
 				"address = '{$this -> username}' 
 				AND 
 				password = MD5( '{$this -> password}' )" 
-			)
-		
+			)		
 			-> limit( 1 );
 		
 		$response -> execute();
-			
+		
+		//- Get response -//
 		$userData = $response->fetchArray();
 
 		//- Test response from DB -//
 		if( count( $userData ) === 1 )
 		{
-			//- Get data -//
+			//- Set structuration data -//
 			$this -> userData = array(
 				'id'		=> $userData[ 0 ][ 'id' ], 
 				'username'	=> $userData[ 0 ][ 'username' ], 
@@ -97,7 +97,8 @@ class Auth_Adapter
 			}
 	}
 	
-	public function getUserData()// : array
+	//- Get params about authenticate user -//
+	public function getUserData()// : array.asotiation
 	{
 		return $this -> userData;
 	}
